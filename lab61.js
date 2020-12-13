e1ba2c567a6e9bd3101aef32d610879a62956e47
Yuliya <74156927+18Yuliya18@users.noreply.github.com>
2020-12-13 12:18:40 +0200

    Delete lab61.js

--- lab61.js
+++
@@ -1,9 +0,0 @@
-alert("Love calculator!");
-var yourName1=prompt("Enter name1");
-var yourName2=prompt("Enter name2");
-
-function getRandomInt(min, max)
-{
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-alert ("Love score:" + getRandomInt(1, 100)  +"%");
