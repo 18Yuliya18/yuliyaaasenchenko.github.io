cd9f38f0d77bda604488895fa5c3acceffc7d207
Yuliya <74156927+18Yuliya18@users.noreply.github.com>
2020-12-13 12:22:07 +0200

    Delete lab64.js

--- lab64.js
+++
@@ -1,7 +0,0 @@
-function who(buyers)
-{
-  var  buyers= Array("Yuliya", "Di", "Liza", "Alexey", "Ann", "Daniel");
-  var buyer =buyers [Math.floor(Math.random()*buyers.length)];
-  console.log("Who’s buying Coffee?")
-  return buyer
-}
